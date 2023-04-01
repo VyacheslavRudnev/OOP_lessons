0a9bc448d6ed5a4276997bf8e9155c7c615c2ef8
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-04-01 11:52:46 +0300

    class Book is done

--- application.cpp
+++ application.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
-#include <string> 
-#include <windows.h> 
-
-using namespace std;
+#include "library.h"
 
 class Book
 {
@@ -75,18 +71,34 @@ public:
 		setIsAvailable();
 	}
 	
-	/*Book(string title,string author)
+	Book(string author):Book()
 	{
-		setTitle(title);
 		setAuthor(author);
+		setYear(2023);
+	}
+
+	Book(string author, int year) : Book(author)
+	{
+		setAuthor(author);
+		setYear(2020);
+		
+	}
+
+	explicit Book(const Book & book)
+	{
+		setTitle(book.title);
+		setAuthor(book.author);
+		setYear(book.year);
+		setIsAvailable();
+	}
+
+	~Book()
+	{
+		cout << " Delete object! " << endl;
+		setTitle(" ");
+		setAuthor(" ");
 		setYear(0);
 		setIsAvailable();
-	}*/
-
-	Book(string title, string author) : Book()
-	{
-		setTitle(title);
-		setAuthor(author);
 	}
 
 
@@ -95,22 +107,23 @@ public:
 
 int main()
 {
-	Book a, b("BOOK", "Rudnev");
-	/*a.getTitle();
-	a.getAuthor();
-	a.getYear();
-	a.getIsAvailable();*/
+	Book a, b("Rudnev", 2022);
+	Book a_copy(a);
 
-	cout << "a Title " << a.getTitle() << endl;
-	cout << "a Author " << a.getAuthor() << endl;
-	cout << "a Year " << a.getYear() << endl;
-	cout << "a IsAvailable " << a.getIsAvailable() << endl;
+	cout << "a Title : " << a.getTitle() << endl;
+	cout << "a Author : " << a.getAuthor() << endl;
+	cout << "a Year : " << a.getYear() << endl;
+	cout << "a IsAvailable : " << a.getIsAvailable() << endl;
 	cout << "_____________________" << endl;
-	cout << "b Title " << b.getTitle() << endl;
-	cout << "b Author " << b.getAuthor() << endl;
-	cout << "b Year " << b.getYear() << endl;
-	cout << "b IsAvailable " << b.getIsAvailable() << endl;
-	
+	cout << "b Title : " << b.getTitle() << endl;
+	cout << "b Author : " << b.getAuthor() << endl;
+	cout << "b Year : " << b.getYear() << endl;
+	cout << "b IsAvailable : " << b.getIsAvailable() << endl;
+	cout << "_____________________" << endl;
+	cout << "a_copy Title : " << a_copy.getTitle() << endl;
+	cout << "a_copy Author : " << a_copy.getAuthor() << endl;
+	cout << "a_copy Year : " << a_copy.getYear() << endl;
+	cout << "a_copy IsAvailable : " << a_copy.getIsAvailable() << endl;
 	
 	return 0;
 }
